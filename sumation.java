fe17c7e49ce094e000be007854bdc17c053a28c2
vrushabhmagdum8108@gmail.com <vrushabhmagdum8108@gmail.com>
2023-02-27 12:44:21 +0530

    initial commit

---
+++ sumation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class sumation{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 2 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("sumation = "+sum);
+    }
+}
+
+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
